018dc838dc8ed85de7771d2f3b6fc97b7b158694
dennergazevedo <dennergazevedo@gmail.com>
2020-11-25 08:46:08 -0300

    SCREEN CORRECT

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -49,7 +49,7 @@ export default function Navbar() {
             </Container>
             <Container>
                 {
-                    screen.width > 800?
+                    screen !== undefined && screen.width > 800?
                     <Menu>
                         <Switch 
                             onChange={toggleTheme} 
